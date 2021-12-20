3f04ed0184dc60eb49a9385a73f8c2b91c102d6c
yusufyp <yusuf.yudhistirazhy@gmail.com>
2021-12-20 13:20:54 +0700

    Use Context

--- 04-starting-project/src/components/Login/Login.js
+++ 04-starting-project/src/components/Login/Login.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useReducer } from "react";
-
+import React, { useState, useEffect, useReducer, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
@@ -41,6 +41,9 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext);
+
   // variabel hanya untuk cek perubahan validitas email dan password, lebih efisien
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -79,7 +82,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
