9efefd0cdbb6967cb3c8a8396a01e683e476176f
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-03 18:55:03 +0700

    Fetch meals from backend

--- ordering-project/src/components/Meals/AvailableMeals.js
+++ ordering-project/src/components/Meals/AvailableMeals.js
@@ -1,49 +1,60 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState, useCallback } from "react";
+import useHttp from "../../hooks/use-http";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+
+  const transformData = useCallback((taskObject) => {
+    const mealList = [];
+    for (const key in taskObject) {
+      mealList.push({
+        id: key,
+        name: taskObject[key].name,
+        description: taskObject[key].description,
+        price: taskObject[key].price,
+      });
+    }
+    setMeals(mealList);
+  }, []);
+
+  const httpData = useHttp(transformData);
+  const { isLoading, error, sendRequest } = httpData;
+
+  useEffect(() => {
+    sendRequest({
+      url: "https://react-project-eb3a6-default-rtdb.firebaseio.com/meals.json",
+    });
+  }, [sendRequest]);
+
+  let content = <h3>Loading...</h3>;
+
+  if (error) {
+    content = <h3>{error}</h3>;
+  }
+
+  if (!isLoading && meals.length === 0 && !error) {
+    content = <h3>No meals found!</h3>;
+  }
+
+  if (meals.length > 0) {
+    content = meals.map((meal) => (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        <ul>{content}</ul>
       </Card>
     </section>
   );
