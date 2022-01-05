229e992a1eb418bda954c3c4b10db6d8cf35b7d6
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-05 11:35:31 +0700

    Demo auth with redux

--- Redux 1/src/store/index.js
+++ Redux 1/src/store/index.js
@@ -1,14 +1,14 @@
 //import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -21,6 +21,24 @@ const counterSlice = createSlice({
     },
     toggle(state) {
       state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+
+    logout(state) {
+      state.isAuthenticated = false;
     },
   },
 });
@@ -58,9 +76,11 @@ const counterSlice = createSlice({
 // };
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  // untuk di useSelector()
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
