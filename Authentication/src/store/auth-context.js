8b9660015f14d54f6766053e05ec12609a3f83ca
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-12 15:50:04 +0700

    Auto Logout

--- Authentication/src/store/auth-context.js
+++ Authentication/src/store/auth-context.js
@@ -3,9 +3,18 @@ import React, { useState } from "react";
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
-  login: (token) => {},
+  login: (token, expirationTime) => {},
   logout: () => {},
 });
+
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
 
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
@@ -13,13 +22,17 @@ export const AuthContextProvider = (props) => {
 
   const userIsLogin = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+    setTimeout(loginHandler, remainingTime);
   };
 
   const contextValue = {
