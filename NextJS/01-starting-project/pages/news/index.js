1971c6f5834f86ddb78b25622d5434ed6076ee51
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-14 17:50:04 +0700

    Linking Between Pages

--- NextJS/01-starting-project/pages/news/index.js
+++ NextJS/01-starting-project/pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>THe News Page</h1>;
+  return (
+    <Fragment>
+      <h1>THe News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-is-great">NextJS is A Great Framework</Link>
+        </li>
+        <li>
+          <Link href="/news/nuxt-is-great">NuxtJS is A Great Framework</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
