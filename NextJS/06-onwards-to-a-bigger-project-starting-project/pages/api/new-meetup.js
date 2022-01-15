36c6e319f057fd466b23863ddbdfd0d7b58646bb
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-15 20:58:26 +0700

    Basic Mongo DB API Route

--- NextJS/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ NextJS/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,8 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongosh "mongodb+srv://cluster0.cfxhs.mongodb.net/meetups" --username yusuflk9'
+    );
+
+    const db = client.db;
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
