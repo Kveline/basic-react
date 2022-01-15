a11c4b32f1511356e7f1509ca99cef54576dfe0d
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-15 12:47:14 +0700

    Custom Components & CSS Modules

---
+++ NextJS/06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://picsum.photos/200/300"
+      title="A First Meetup"
+      address="Jalan Betawi 1 no 1987"
+      description="This is our first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
