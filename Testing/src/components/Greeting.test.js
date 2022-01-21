ecd3f6c4acba5344cdc8362d2f4d17b1648fbc3c
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-21 18:14:49 +0700

    single component test

---
+++ Testing/src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hello World as Text", () => {
+  // Arrange
+  render(<Greeting />);
+  // Act
+  // nothing..
+
+  // Assert
+  const helloWorldElement = screen.getByText("Hello World", { exact: false });
+  expect(helloWorldElement).toBeInTheDocument();
+});
