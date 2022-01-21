0bf51fb637eca1523481a9c309258410aab15c13
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-21 10:34:00 +0700

    Animation Timing

--- animations--01/src/components/Modal/Modal.js
+++ animations--01/src/components/Modal/Modal.js
@@ -3,9 +3,20 @@ import Transition from "react-transition-group/Transition";
 
 import "./Modal.css";
 
+const animationTiming = {
+  // disamain kayak setingan css animation
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
       {(state) => {
         const cssClass = [
           "Modal",
